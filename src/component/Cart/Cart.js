b5ea51ca8d2c0af63866c2ff98ca0f560ee5fff4
VaidehiRami <ramivaidehi@gmail.com>
2022-05-25 17:47:32 +0530

    SecondCommit

---
+++ src/component/Cart/Cart.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import Modaldtl from '../UI/Modaldtl';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/CartContext';
+
+const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartRemoveHandler.bind(null, item.id)}
+          onAdd={cartAddHandler.bind(null, item)}
+        />
+      ))}
+    </ul>
+  );
+  return (
+    <Modaldtl onClose={props.onClose}>
+    {cartItems}
+    <div className={classes.total}>
+      <span>Total Amount</span>
+      <span>{totalAmount}</span>
+    </div>
+    <div className={classes.actions}>
+      <button className={classes['button--alt']} onClick={props.onClose}>
+        Close
+      </button>
+      {hasItems && <button className={classes.button}>Order</button>}
+    </div>
+  </Modaldtl>
+
+  );
+}
+
+export default Cart
