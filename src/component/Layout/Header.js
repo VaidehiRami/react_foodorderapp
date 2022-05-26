da8431b4ca3112cea9dde098d58f2131bf93b560
VaidehiRami <ramivaidehi@gmail.com>
2022-05-26 13:58:40 +0530

    changes

--- src/component/Layout/Header.js
+++ src/component/Layout/Header.js
@@ -15,7 +15,7 @@ const Header = (props) => {
       <img src={mealsImage} alt='A table full of delicious food!' />
     </div>
   </Fragment>
-  )
+  );
 }
 
-export default Header
+export default Header;
